50387a5dc29590536bdd71540602f08f06d7eb2d
stevemwenje <stephen.mwenje@equitybank.co.ke>
2022-05-17 12:06:31 +0300

    create ship,crew member refactor

--- api/utils.py
+++ api/utils.py
@@ -1,9 +1,4 @@
-from rest_framework import status
-from rest_framework.exceptions import ValidationError
-from rest_framework.response import Response
-
 from .models import Ship, CrewMember
-from .serializers import CrewSerializer, ShipSerializer, MothershipSerializer
 
 
 def create_ship(mothership_id):
