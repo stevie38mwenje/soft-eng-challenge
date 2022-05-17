b9c658767d04569030900240559b6f044a4f2291
stevemwenje <stephen.mwenje@equitybank.co.ke>
2022-05-17 04:10:21 +0300

    added swagger API documentation

--- airforce/urls.py
+++ airforce/urls.py
@@ -15,9 +15,36 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.generators import OpenAPISchemaGenerator
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        schema.schemes = ["http", "https"]
+        return schema
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Mothership API",
+        default_version='v1',
+        description="Human airforce API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    generator_class=BothHttpAndHttpsSchemaGenerator,
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=None), name='schema-swagger-ui'),
 
 ]
