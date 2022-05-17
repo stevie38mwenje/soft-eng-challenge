a83ead723a7dc3215c4de55cbd6e843375ca6b36
stevemwenje <stephen.mwenje@equitybank.co.ke>
2022-05-18 01:16:03 +0300

    swap crew

--- api/utils.py
+++ api/utils.py
@@ -19,7 +19,7 @@ def create_crew(ship_id):
 def swap_crew(from_ship_id, to_ship_id, name):
     from_ship = get_object_or_404(Ship, id=from_ship_id)
     to_ship = get_object_or_404(Ship, id=to_ship_id)
-    if not to_ship.has_vacancy:
+    if not to_ship.has_space:
         raise ValidationError(detail='Not space left for the swap')
     crew = get_object_or_404(CrewMember, ship=from_ship, name=name)
     crew.ship = to_ship
